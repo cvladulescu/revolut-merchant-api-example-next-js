e4d91cb0ebbfc31e254f0db3cfd18b6516fbc13d
cvladulescu <vladulescu.catalin@gmail.com>
2021-02-24 17:04:15 +0200

    payment buttons

--- db/index.js
+++ db/index.js
@@ -7,5 +7,9 @@ export const orders = new PouchDB("orders", { adapter: "memory" });
 export const goods = new PouchDB("goods", { adapter: "memory" });
 
 goods.bulkDocs([
-  { _id: "001", title: "Shoes", amount: 10000, currency: "GBP" }
+  { _id: "001", title: "Basic", amount: 100, currency: "GBP" },
+  { _id: "002", title: "Challange 1", amount: 500, currency: "GBP" },
+  { _id: "003", title: "Challange 2", amount: 1000, currency: "GBP" },
+  { _id: "004", title: "Challange 3", amount: 10000, currency: "GBP" },
+  { _id: "005", title: "Stop the live", amount: 100000, currency: "GBP" }
 ]);
