0ef9628700295935bb83c3e87641fde629b9c32b
cvladulescu <vladulescu.catalin@gmail.com>
2021-03-02 05:11:39 +0200

    modifications

--- pages/checkout.js
+++ pages/checkout.js
@@ -80,7 +80,7 @@ function CheckoutPage({ order }) {
     currency: order.total.currency
   });
 
-  console.log(order.token)
+  console.log(order.token);
   return (
     <>
       <h2>Checkout ({sum})</h2>
